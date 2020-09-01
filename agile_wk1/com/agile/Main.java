d376d3d663dabb4bbf1df4a047a718126a6ab508
Rick <shariq1989@gmail.com>
2020-09-01 15:40:20 -0600

    check in HW1 progress

---
+++ agile_wk1/com/agile/Main.java
@@ -0,0 +1,16 @@
+package com.agile;
+
+/**
+ * This program prints a passed-in argument
+ * in the format prescribed in HW1's rubric.
+ *
+ * Ex. if "Richard" was passed in,
+ * the program will print "Hello, Richard!"
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // print "Hello, args[0]!"
+        System.out.println("Hello, " + args[0] + "!");
+    }
+}
